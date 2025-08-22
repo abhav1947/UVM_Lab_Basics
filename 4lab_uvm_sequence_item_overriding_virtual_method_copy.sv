ec0c10e842d912fc120d55451cfd855b8a620883
abhav1947 <66078840+abhav1947@users.noreply.github.com>
2025-08-22 19:29:52 +0530

    Update 4lab_uvm_sequence_item_overriding_virtual_method_copy.sv

--- 4lab_uvm_sequence_item_overriding_virtual_method_copy.sv
+++ 4lab_uvm_sequence_item_overriding_virtual_method_copy.sv
@@ -1,4 +1,33 @@
 // Overriding virtual functions, so that we dont register variables with factory
+/*
+You have a student record form with two sections:
+Top part (base class, already designed by school):
+Roll number
+Class
+Section
+
+Bottom part (your extension):
+Marks in Maths (data1)
+Marks in Science (data2)
+Marks in English (data3)\
+
+Now you want to make a photocopy of one student’s form into another student’s form.
+Case 1: If you only copy the bottom part
+You write down the Maths, Science, English marks into the new form.
+But you forgot to copy Roll number, Class, Section from the top part.
+So the copied form is incomplete (no identity, only marks).
+
+Case 2: If you only copy the top part
+You use the school’s built-in photocopy machine (super.do_copy).
+That machine only knows about Roll number, Class, Section (the top part).
+It does not know about your custom fields (marks).
+So you end up with a copy that has Roll number but zero marks.
+
+Correct Way: Combine both
+First use the school’s photocopy machine (super.do_copy) → this copies all the important base details.
+Then manually write the marks (your custom fields) into the new form.
+That way, the new student form (_t2) becomes a complete twin of the original (_t1).
+*/
 class write_txn extends uvm_sequence_item;
   `uvm_object_utils(write_txn)
   randc bit [3:0] data1, data2, data3;
